a00f1ccdf44558c459d0e3859b428ecf95893839
eacarter <eacarter@mtu.edu>
2015-11-16 16:20:36 -0500

    initial items

---
+++ js/slideshow.js
@@ -0,0 +1,289 @@
+﻿//var TheSlideshow = new Slideshow('.slideshow', '.slide-outer', '.slide-container', '.slideshow-card', 640, 1024, 3, 2, 1);
+
+// SLIDESHOW CLASS
+function Slideshow(slideshowSelector, outerContainerSelector, containerSelector, cardSelector, medSmallBreak, largeMedBreak, largeNumCards, medNumCards, smallNumCards, animationTime) {
+    var _self = this;
+    this.SlideshowSelector = slideshowSelector;
+    this.OuterContainerSelector = outerContainerSelector;
+    this.ContainerSelector = containerSelector;
+    this.CardSelector = cardSelector;
+    this.MedSmallBreak = medSmallBreak;
+    this.LargeMedBreak = largeMedBreak;
+    this.AnimationTime = (animationTime ? animationTime : 300);
+
+    this.LargeContainer = new Object(), this.MedContainer = new Object(), this.SmallContainer = new Object();
+
+    this.setContainerVals = function (container, numCards) {
+        switch (numCards) {
+            case 4:
+                container.columnWidth = 2;
+                container.columnOffset = 0;
+                container.containerLeft = "25%";
+                container.cardWidth = "25%";
+                container.rowWidth = "150%";
+                break;
+            case 3:
+                container.columnWidth = 2;
+                container.columnOffset = 2;
+                container.containerLeft = "66.666667%";
+                container.cardWidth = "33.3333%";
+                container.rowWidth = "200%";
+                break;
+            case 2:
+                container.columnWidth = 3;
+                container.columnOffset = 0;
+                container.containerLeft = "50%";
+                container.cardWidth = "50%";
+                container.rowWidth = "200%";
+                break;
+            case 1:
+                container.columnWidth = 4;
+                container.columnOffset = 0;
+                container.containerLeft = "100%";
+                container.cardWidth = "100%";
+                container.rowWidth = "300%";
+                break;
+        }
+        container.NumCards = numCards;
+    }
+
+    this.setContainerVals(this.LargeContainer, largeNumCards);
+    this.setContainerVals(this.MedContainer, medNumCards);
+    this.setContainerVals(this.SmallContainer, smallNumCards);
+
+    this.CurContainer;// = this.LargeContainer;
+    this.CardIndex = 0;
+    this.NumCards;
+
+    this.CardsShowing;
+    this.CardsArray;
+    this.EmptyCardLeft;
+    this.EmptyCardRight;
+
+    this.LeftMostCard = 0;
+
+    this.SizeEnum = { SMALL: 1, MEDIUM: 2, LARGE: 3 };
+    this.CurSize;
+
+    this.slideshowify = function () {
+        $(window).resize(function () {
+            _self.resizeSlideshow();
+        });
+        $(document).ready(function () {
+            _self.CardsArray = $(_self.CardSelector);
+
+            _self.duplicateCards();
+
+            //_self.assembleCards();
+            _self.resizeSlideshow();
+
+            _self.initSliding();
+        });
+        //this.resizeSlideshow();	
+    }
+
+    this.duplicateCards = function () {
+        var slideshowRow = $(this.SlideshowSelector + ".row");
+        var numCards = this.CardsArray.length;
+        this.NumCards = numCards;
+
+        $(this.CardsArray[numCards - 1]).clone().prependTo(slideshowRow);
+        for (var i = 0; i < numCards; i++) {
+            $(this.CardsArray[i]).clone().appendTo(slideshowRow);
+        }
+    }
+
+    this.assembleCards = function () {
+        _self.CardsArray = $(_self.CardSelector);
+
+        var curWidth = window.innerWidth;
+        var first = true;
+
+        if (curWidth > largeMedBreak)
+            _self.CurSize = _self.SizeEnum.LARGE;
+        else if (curWidth > medSmallBreak)
+            _self.CurSize = _self.SizeEnum.MEDIUM;
+        else
+            _self.CurSize = _self.SizeEnum.SMALL;
+
+        for (var i = 0; i < _self.CardsArray.length; i++) {
+            $(_self.CardsArray[i]).removeClass('large-offset-' + _self.LargeContainer.columnOffset); //remove by default
+            $(_self.CardsArray[i]).removeClass('medium-offset-' + _self.MedContainer.columnOffset); //remove by default
+            $(_self.CardsArray[i]).removeClass('small-offset-' + _self.SmallContainer.columnOffset); //remove by default
+            $(_self.CardsArray[i]).removeClass('ghosted'); //remove by default
+            $(_self.CardsArray[i]).removeClass('ghosted'); //remove by default
+            $(_self.CardsArray[i]).css('opacity', ''); //remove element opacity settings after animation
+
+            if ((i >= _self.CardIndex) && (i < (_self.CardIndex + _self.CurContainer.NumCards + 2))) {
+                $(_self.CardsArray[i]).addClass('large-' + _self.LargeContainer.columnWidth)
+                $(_self.CardsArray[i]).addClass('medium-' + _self.MedContainer.columnWidth)
+                $(_self.CardsArray[i]).addClass('small-' + _self.SmallContainer.columnWidth)
+
+                if (first) {
+                    first = false;
+                    $(_self.CardsArray[i]).addClass('large-offset-' + _self.LargeContainer.columnOffset);
+                    $(_self.CardsArray[i]).addClass('medium-offset-' + _self.MedContainer.columnOffset);
+                    $(_self.CardsArray[i]).addClass('small-offset-' + _self.SmallContainer.columnOffset);
+
+                    $(_self.CardsArray[i]).addClass('ghosted');
+                }
+
+                if (i == _self.CardIndex + _self.CurContainer.NumCards + 1) {
+                    $(_self.CardsArray[i]).addClass('ghosted');
+                }
+
+                $(_self.CardsArray[i]).addClass('columns');
+                $(_self.CardsArray[i]).removeClass('card-hide');
+            }
+            else {
+                $(_self.CardsArray[i]).removeClass('columns');
+                $(_self.CardsArray[i]).addClass('card-hide');
+            }
+        }
+
+        $(_self.SlideshowSelector + ".row").css('width', _self.CurContainer.rowWidth);
+        $(_self.SlideshowSelector).css('max-width', _self.CurContainer.rowWidth);
+        $(_self.SlideshowSelector).css('left', "-" + _self.CurContainer.containerLeft);
+        //$(_self.ContainerSelector).css('width', _self.LargeContainer.containerWidth);
+    }
+
+    this.isMobile = function () {
+        // need to implement this.	
+        var check = false;
+        (function (a, b) { if (/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino/i.test(a) || /1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0, 4))) check = true })(navigator.userAgent || navigator.vendor || window.opera);
+        return check;
+    }
+
+    this.initSliding = function () {
+        if (!this.isMobile()) {
+            this.drawArrows();
+        }
+        else {
+            this.drawArrows();
+            this.enableSwipes();
+        }
+    }
+
+    this.enableSwipes = function () {
+        // do we have jquery mobile??
+        if ($.mobile) {
+            //jq mobile loaded
+        } else {
+            // let's include it. this doesn't work quite right yet.
+            //loadjscssfile("http://code.jquery.com/mobile/1.4.5/jquery.mobile-1.4.5.min.js","js");
+        }
+
+        $(this.SlideshowSelector).on("swiperight", function () {
+            _self.SlideRight();
+        });
+        $(this.SlideshowSelector).on("swipeleft", function () {
+            _self.SlideLeft();
+        });
+    }
+
+    this.drawArrows = function () {
+        var rightArrowHtml = "<div class='slideshow-arrow' id='slideshow-arrow-right'></div>";
+
+        $(this.OuterContainerSelector).append(rightArrowHtml);
+        $("#slideshow-arrow-right").click(function () {
+            //_self.SlideRight();
+            _self.SlideLeft();
+        });
+
+        var leftArrowHtml = "<div class='slideshow-arrow' id='slideshow-arrow-left'></div>";
+
+        $(this.OuterContainerSelector).append(leftArrowHtml);
+        $("#slideshow-arrow-left").click(function () {
+            //_self.SlideLeft();
+            _self.SlideRight();
+        });
+    }
+
+    this.resizeSlideshow = function () {
+        var windowSize = this.getMediaState();
+        if (windowSize == this.CurSize) return; //short-circuit if size has not changed;
+
+        this.CurSize = windowSize;
+
+        switch (windowSize) {
+            case this.SizeEnum.LARGE:
+                this.CurContainer = this.LargeContainer;
+                break;
+            case this.SizeEnum.MEDIUM:
+                this.CurContainer = this.MedContainer;
+                break;
+            case this.SizeEnum.SMALL:
+                this.CurContainer = this.SmallContainer;
+                break;
+        }
+
+        this.assembleCards();
+
+        /*var cardWidth = $(this.SlideshowSelector).width()/this.getMediaState();
+		$(this.CardSelector).css('width', cardWidth + 'px');
+		var containerWidth = $(this.CardSelector).children().length*$(this.CardSelector).width();
+		$(this.ContainerSelector).css('width',containerWidth + 'px');
+		// need to change marginLeft. do we know what the size was?
+		var newMargin = -cardWidth*this.LeftMostCard;
+		$(this.ContainerSelector).css("margin-left",newMargin);*/
+    }
+
+    this.SlideLeft = function () {
+        $(this.CardsArray[this.CardIndex + 1]).animate({
+            opacity: '-=' + 1
+        }, this.AnimationTime);
+
+        $(this.CardsArray[this.CardIndex + this.CurContainer.NumCards + 1]).animate({
+            opacity: '+=' + 1
+        }, this.AnimationTime);
+
+        this.CardIndex = (this.CardIndex + 1) % this.NumCards;
+
+        $(this.SlideshowSelector).animate({
+            left: '-=' + this.CurContainer.cardWidth
+        }, this.AnimationTime, "swing", this.assembleCards);
+    }
+
+    this.SlideRight = function () {
+        $(this.CardsArray[this.CardIndex + this.CurContainer.NumCards]).animate({
+            opacity: '-=' + 1
+        }, this.AnimationTime);
+
+        $(this.CardsArray[this.CardIndex]).animate({
+            opacity: '+=' + 1
+        }, this.AnimationTime);
+
+        this.CardIndex = (this.CardIndex + this.NumCards - 1) % this.NumCards;
+
+        $(this.SlideshowSelector).animate({
+            left: '+=' + this.CurContainer.cardWidth
+        }, this.AnimationTime, "swing", this.assembleCards);
+    }
+    // returns 1 - small, 2 - medium, 3 - large
+    this.getMediaState = function () {
+        if (window.innerWidth > this.LargeMedBreak) return 3;
+        if (window.innerWidth > this.MedSmallBreak) return 2;
+        return 1;
+    }
+
+    this.slideshowify(this.ContainerSelector);
+}
+
+// HELPER FUNCTION
+
+function loadjscssfile(filename, filetype) {
+    if (filetype == "js") { //if filename is a external JavaScript file
+        var fileref = document.createElement('script')
+        fileref.setAttribute("type", "text/javascript")
+        fileref.setAttribute("src", filename)
+    }
+    else if (filetype == "css") { //if filename is an external CSS file
+        var fileref = document.createElement("link")
+        fileref.setAttribute("rel", "stylesheet")
+        fileref.setAttribute("type", "text/css")
+        fileref.setAttribute("href", filename)
+    }
+
+    if (typeof fileref != "undefined")
+        document.getElementsByTagName("head")[0].appendChild(fileref)
+}
---
+++ js/sparkr.js
@@ -0,0 +1,460 @@
+﻿var pulseInterval;
+
+var cgMode = "fix";
+var enableChange = false;
+var SCREEN_MODE_ENUM = { "LARGE": 3, "MEDIUM": 2, "SMALL": 1 };
+var SCREEN_MODE_NAME = { 1: "small", 2: "medium", 3: "large" };
+var screenMode;
+var frames, curFrameIndex, isScrolling = false;
+var SCROLL_TIME = 700;
+var SCROLL_DIST = 150;
+var SCROLL_INT = 50;
+var sparkx, sparky, endx, initSparkx;
+var curXD = 5;
+var segments = new Array();
+var mInt;
+//var curChange = .05, lightMax = 1.8, lightMin = .9;
+var curChange = 1, lightMax = 50, lightMin = 30;
+
+$(document).ready(function () {
+    screenMode = getScreenMode();
+
+    if (screenMode == SCREEN_MODE_ENUM.LARGE) {
+        pulseInterval = window.setInterval(function () { pulseLight(); }, 50);
+    }
+
+    findSegments();
+
+    initSparkx = parseInt($('#spark').attr('cx'));
+    sparkx = initSparkx;
+
+    var mousewheelevt = (/Firefox/i.test(navigator.userAgent)) ? "DOMMouseScroll" : "mousewheel"; //FF doesn't recognize mousewheel as of FF3.x
+    $(document).bind(mousewheelevt, function (e) {
+        var evt = window.event || e //equalize event object     
+        evt = evt.originalEvent ? evt.originalEvent : evt; //convert to originalEvent if possible               
+        var delta = evt.detail ? evt.detail * (-40) : evt.wheelDelta //check for detail first, because it is used by Opera and FF
+        if (delta > 0) {
+            prevFrame();
+        }
+        else {
+            nextFrame();
+        }
+
+        return false;
+    });
+
+    $(window).bind('swiperight', prevFrame);
+    $(window).bind('swipeleft', nextFrame);
+    $(window).bind('swipedown', nextFrame);
+    $(window).bind('swipeup', nextFrame);
+
+    document.ontouchmove = function (e) { e.preventDefault() };
+
+
+    $(window).resize(function () {
+        frameHandleResize();
+    });
+
+    $(window).bind('resize', verifyState);
+
+    findFrames();
+
+    var icons = $('.cgIcon');
+    for (var i = 0; i < icons.length; i++) {
+        $(icons[i]).attr('index', i % frames.length);
+    }
+
+    $('.cgIcon').click(function () {
+        jumpToFrame(this);
+    });
+});
+
+$(window).load(function () {
+    $('.fill-text-width').textfill({ maxFontPixels: 200, widthOnly: true, complete: function () { $('.fill-text-width').removeClass('transparent'); } });
+});
+
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+debounce = function (func, wait, immediate) {
+    var timeout, args, context, timestamp, result;
+
+    var later = function () {
+        var last = now() - timestamp;
+
+        if (last < wait && last >= 0) {
+            timeout = setTimeout(later, wait - last);
+        } else {
+            timeout = null;
+            if (!immediate) {
+                result = func.apply(context, args);
+                if (!timeout) context = args = null;
+            }
+        }
+    };
+
+    return function () {
+        context = this;
+        args = arguments;
+        timestamp = now();
+        var callNow = immediate && !timeout;
+        if (!timeout) timeout = setTimeout(later, wait);
+        if (callNow) {
+            result = func.apply(context, args);
+            context = args = null;
+        }
+
+        return result;
+    };
+};
+
+now = Date.now || function () {
+    return new Date().getTime();
+};
+
+refillText = debounce(function () {
+    $('.fill-text-width').textfill({ maxFontPixels: 200, widthOnly: true });
+}, 500);
+
+findFrames = function () {
+    curFrameIndex = 0;
+    frames = $('div.frame');
+};
+
+frameHandleResize = function () {
+    var frameSize = $(frames[0]).innerWidth();
+    $('#mainContent').css('left', "-" + (frameSize * curFrameIndex) + "px");
+
+    refillText();
+    //$('.fill-text-width').textfill({ maxFontPixels: 200, widthOnly: true });
+    //alert('resized?');
+};
+
+
+
+restoreSlides = function () {
+    frames.css('margin-left', '');
+    frames.css('margin-right', '');
+    frames.css('display', '');
+
+    var frameSize = $(frames[0]).innerWidth();
+    $('#mainContent').css('left', "-" + (frameSize * curFrameIndex) + "px");
+};
+
+verifyState = function () {
+    if (isScrolling) return;
+
+    resetSpark();
+
+    var curScreenMode = getScreenMode();
+
+    if (curScreenMode == screenMode) return;
+
+    hideScreenMode(screenMode);
+
+    screenMode = curScreenMode;
+
+    showScreenMode(screenMode);
+};
+
+resetSpark = function () {
+    var adjSparkx;
+
+    //reset sparkx
+    adjSparkx = initSparkx + (SCROLL_DIST * curFrameIndex);
+
+    if (sparkx == adjSparkx) return;
+
+    setCgPosition(curFrameIndex);
+};
+
+getScreenMode = function () {
+    var width = window.innerWidth;
+
+    if (width > 1024) {
+        return SCREEN_MODE_ENUM.LARGE;
+    } else if (width > 640) {
+        return SCREEN_MODE_ENUM.MEDIUM;
+    } else {
+        return SCREEN_MODE_ENUM.SMALL;
+    }
+};
+
+hideScreenMode = function (screenMode) {
+    if (screenMode == SCREEN_MODE_ENUM.LARGE) {
+        disableCardiogram = true;
+        window.clearInterval(pulseInterval);
+    } else {
+        //
+    }
+};
+
+showScreenMode = function (screenMode) {
+    if (screenMode == SCREEN_MODE_ENUM.LARGE) {
+        pulseInterval = window.setInterval(function () { pulseLight(); }, 50);
+    } else {
+
+    }
+};
+
+jumpToFrame = function (elem) {
+    if (isScrolling) return;
+
+    var jumpFrame = parseInt($(elem).attr('index'));
+    var frameSize = $(frames[0]).innerWidth();
+    var frameDiff;
+    if (curFrameIndex == jumpFrame) return;
+    isScrolling = true;
+
+    if (curFrameIndex > jumpFrame) {
+        frameDiff = curFrameIndex - jumpFrame;
+
+        if (screenMode == SCREEN_MODE_ENUM.LARGE) {
+            if (frameDiff == 1) {
+                cgToLeft(frameDiff);
+                activateIcons(jumpFrame);
+            } else {
+                activateIcons(jumpFrame);
+                setCgPosition(jumpFrame);
+            }
+        } else {
+            activateIcons(jumpFrame);
+        }
+
+        var move = $(frames[0]).innerWidth();
+
+        $(frames[jumpFrame]).css('margin-left', (frameSize * (frameDiff - 1)) + "px");
+        for (var i = 1; i < frameDiff; i++) {
+            $(frames[curFrameIndex - i]).css('display', 'none');
+        }
+
+        $('#mainContent').animate({
+            left: '+=' + move + 'px'
+        }, SCROLL_TIME, "swing", function () { isScrolling = false; restoreSlides(); verifyState(); });
+    } else {
+        frameDiff = jumpFrame - curFrameIndex;
+
+        if (screenMode == SCREEN_MODE_ENUM.LARGE) {
+            if (frameDiff == 1) {
+                cgToRight(frameDiff);
+                activateIcons(jumpFrame);
+            } else {
+                activateIcons(jumpFrame);
+                setCgPosition(jumpFrame);
+            }
+        } else {
+            activateIcons(jumpFrame);
+        }
+
+        var move = $(frames[0]).innerWidth();
+
+        $(frames[jumpFrame]).css('margin-right', (frameSize * (frameDiff - 1)) + "px");
+        for (var i = 1; i < frameDiff; i++) {
+            $(frames[curFrameIndex + i]).css('display', 'none');
+        }
+
+        $('#mainContent').animate({
+            left: '-=' + move + 'px'
+        }, SCROLL_TIME, "swing", function () { isScrolling = false; restoreSlides(); verifyState(); });
+    }
+
+    curFrameIndex = jumpFrame;
+};
+
+activateIcons = function (goalIcon) {
+    for (var i = 0; i < frames.length; i++) {
+        if (i == goalIcon) {
+            $('.cgIcon[index=' + i + ']').addClass('current');
+        } else {
+            $('.cgIcon[index=' + i + ']').removeClass('current');
+        }
+
+        if (i <= goalIcon) {
+            $('.cgIcon[index=' + i + ']').addClass('active');
+        } else {
+            $('.cgIcon[index=' + i + ']').removeClass('active');
+        }
+    }
+};
+
+prevFrame = function () {
+    if (curFrameIndex == 0) return;
+    if (isScrolling) return; //disable concurrent scrolls
+
+    curFrameIndex--;
+    isScrolling = true;
+
+    var move = $(frames[0]).innerWidth();
+
+    if (screenMode == SCREEN_MODE_ENUM.LARGE) {
+        cgToLeft(1);  //animate cardiogram pulse
+    }
+
+    activateIcons(curFrameIndex);
+
+    $('#mainContent').animate({
+        left: '+=' + move + 'px'
+    }, SCROLL_TIME, "swing", function () { isScrolling = false; restoreSlides(); verifyState(); });
+};
+
+nextFrame = function () {
+    if (curFrameIndex >= frames.length - 1) return; //don't allow scroll past end of page
+    if (isScrolling) return; //disable concurrent scrolls
+
+    curFrameIndex++;
+    isScrolling = true;
+
+    var move = $(frames[0]).innerWidth();
+
+    if (screenMode == SCREEN_MODE_ENUM.LARGE) {
+        cgToRight(1);  //animate cardiogram pulse
+    }
+
+    activateIcons(curFrameIndex);
+
+    $('#mainContent').animate({
+        left: '-=' + move + 'px'
+    }, SCROLL_TIME, "swing", function () { isScrolling = false; restoreSlides(); verifyState(); });
+};
+
+cgToLeft = function (numFrames) {
+    //var scrollDist = window.innerWidth * .2;   //hack; hardcoding
+    curXD = -1 * SCROLL_DIST / (SCROLL_TIME / SCROLL_INT);  //flip left
+    endx = sparkx - (SCROLL_DIST * numFrames);
+
+    mInt = window.setInterval(function () { timedMove(); }, SCROLL_INT);
+};
+
+cgToRight = function (numFrames) {
+    //var scrollDist = window.innerWidth * .2;   //hack; hardcoding
+    curXD = SCROLL_DIST / (SCROLL_TIME / SCROLL_INT);
+    endx = sparkx + (SCROLL_DIST * numFrames);
+
+    mInt = window.setInterval(function () { timedMove(); }, SCROLL_INT);
+};
+
+$(document).keydown(function (e) {
+    switch (e.which) {
+        case 37: //up
+            prevFrame();
+            break;
+        case 39: //down
+            nextFrame();
+            break;
+        case 38: //up
+            prevFrame();
+            break;
+        case 40: //down
+            nextFrame();
+            break;
+        case 83: //s
+            SCROLL_TIME = parseInt(prompt("Scroll time in milliseconds", SCROLL_TIME));
+            break;
+        default: return; //exit this handler for other keys
+    }
+
+    e.preventDefault();
+});
+
+findSegments = function () {
+    var lines = $('#cardiogramLines > line');
+    segments = new Array();
+
+    for (var i = 0; i < lines.length; i++) {
+        segments.push({ x1: parseInt($(lines[i]).attr('x1')), x2: parseInt($(lines[i]).attr('x2')), y1: parseInt($(lines[i]).attr('y1')), y2: parseInt($(lines[i]).attr('y2')) });
+    }
+};
+
+startMove = function () {
+    mInt = window.setInterval(function () { timedMove(); }, 45);
+};
+
+endMove = function () {
+    window.clearInterval(mInt);
+};
+
+timedMove = function () {
+    if (Math.abs(sparkx - endx) < Math.abs(curXD)) {
+        sparkx = endx;
+        window.clearInterval(mInt);
+    } else {
+        sparkx += curXD;
+    }
+
+    fillLineSegments();
+
+    setSparkPos(sparkx);
+};
+
+setCgPosition = function (frameIndex) {
+    sparkx = initSparkx + (SCROLL_DIST * frameIndex);
+    curXD = 0;
+
+    fillLineSegments();
+    setSparkPos(sparkx);
+};
+
+setSparkPos = function (px) {
+    $('#fepointlight').attr('x', px);
+    $('#spark').attr('cx', px);
+};
+
+fillLineSegments = function () {
+    var curClass;
+
+    for (var i = 0; i < segments.length; i++) {
+        curClass = $($('#cardiogramLines line')[i]).attr('class');
+        if (curClass == 'hide') { continue; }
+
+        if (sparkx > segments[i].x1) { //show if sparkx is greater than left side of line
+            $($('#cardiogramLines line')[i]).attr('class', 'red'); //*only* class is 'red'
+            //$('#cardiogramLines line')[i].classList.add('red');
+            //$('#cardiogramLines line')[i].classList.remove('gray');
+        } else {
+            $($('#cardiogramLines line')[i]).attr('class', 'gray'); //*only class is 'gray'
+            //$('#cardiogramLines line')[i].classList.add('gray');
+            //$('#cardiogramLines line')[i].classList.remove('red');
+        }
+    }
+};
+
+moveSpark = function (xd, yd, zd) {
+    curx = parseInt($('#fepointlight').attr('x')) + xd;
+    cury = parseInt($('#fepointlight').attr('y')) + yd;
+    curz = parseInt($('#fepointlight').attr('z')) + zd;
+    //$('#spark').attr(curx + 'px',cury + 'px');
+    $('#fepointlight').attr('x', curx);
+    $('#fepointlight').attr('y', cury);
+    $('#fepointlight').attr('z', curz);
+    $('#spark').attr('cx', curx);
+    $('#spark').attr('cy', cury);
+};
+
+pulseLight = function () {
+    //var curValue = $('.fediffuselight')[0].diffuseConstant.baseVal;
+    var curValue = getSpecularVal();
+
+    if (curValue == null) { window.clearInterval(pulseInterval); return; } //if we fail to retrieve the specular value, remove the interval; incompatible
+
+    if (curChange > 0 && curValue >= lightMax) {
+        curChange *= -1;
+    }
+    else if (curChange < 0 && curValue <= lightMin) {
+        curChange *= -1;
+    }
+
+    var lights = $('.fediffuselight');
+    for (var i = 0; i < lights.length; i++) {
+        //lights[i].diffuseConstant.baseVal = curValue + curChange;
+        lights[i].specularExponent.baseVal = curValue + curChange;
+    }
+};
+
+getSpecularVal = function () {
+    if ($('.fediffuselight')[0].specularExponent) {
+        return $('.fediffuselight')[0].specularExponent.baseVal;
+    }
+    
+    return null;
+};
